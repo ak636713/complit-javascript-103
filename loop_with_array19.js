9b9ccae62f99eae2c730608cc0f3acc7b78dbea1
codingwalaladkak <codingwalaladaka@gmail.com>
2023-08-18 05:59:43 +0530

    loop_array9.js

---
+++ loop_with_array19.js
@@ -0,0 +1,27 @@
+let num = [3, 5, 4, 84, 216]
+
+// for(let i=0; i<num.length; i++){
+//     console.log(num[i])
+// }
+
+//for each loop
+// num.forEach((element)=>{
+//     console.log(element * element)
+// })
+
+
+//array from 
+let Name = "Ajay"
+let arr = Array.from(Name)
+console.log(arr)
+
+//for...of
+// for(let i of num){
+//  console.log(i)   
+// }
+
+
+
+for(let i in num){
+    console.log(num[i])
+}
