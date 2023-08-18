fa18f17aa724cca1f695bd07718241750dc41140
codingwalaladkak <codingwalaladaka@gmail.com>
2023-08-18 05:57:17 +0530

    string-methods-4.js

---
+++ string-methods-14.js
@@ -0,0 +1,29 @@
+// let Name = "Ajay"
+
+// console.log(Name.toLowerCase())
+// console.log(Name.toUpperCase())
+// console.log(Name.slice(2,4))
+// console.log(Name.slice(2))
+
+// let Name1 = "Ajay bhai"
+// let NameNew 
+// NameNew = Name1.replace("bhai", "bro")
+// console.log(NameNew)
+
+// let friend = "rdx"
+
+// console.log(Name.concat(" friend of ", friend, " love you bro"))
+
+// let a = "    raj    "
+// console.log(a)
+// console.log(a.trim())
+
+
+let Name = "Arjun"
+
+for (let i = 0; i < 5; i++) {
+    console.log(Name[i])
+}
+console.log(Name)
+
+
