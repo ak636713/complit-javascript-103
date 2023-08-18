99cf694fd57801370e9b9603c3e8e4fea36b50c1
codingwalaladkak <codingwalaladaka@gmail.com>
2023-08-18 05:31:05 +0530

    array-method18.js

---
+++ array-method18.js
@@ -0,0 +1,34 @@
+//delete method
+// let num = [1, 2, 3, 4, 5]
+// let num_more = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let num_more_a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// // console.log(num)
+// // delete num[2]
+// // console.log(num)
+
+// concat mehtod
+// let newArray = num.concat(num_more)
+// console.log(newArray)
+
+// //sort methods
+// let compare = (a, b) =>{
+//     return a - b;
+// }
+// let num = [565, 654, 45, 25, 12, 45, 25, 12, 45, 25, 12, 45, 25, 12]
+// num.sort(compare)
+// console.log(num)
+
+
+//reverc function
+// num.reverse()
+// console.log(num)
+
+// splice() function
+// let num = [565, 654, 45, 25, 12, 45, 25, 12, 45, 25, 12, 45, 25, 12]
+// let deletedValuse = num.splice(4, 5, 2250, 1204, 2521)
+// console.log(typeof deletedValuse)
+// console.log(num)
+
+let num = [565, 654, 45, 25, 12, 45, 25, 12, 45, 25, 12, 45, 25, 12]
+let newNum = num.slice(3)
+console.log(newNum)
