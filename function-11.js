6fc37b17401c9f0ec28d3f24a71b6862dd956d9d
codingwalaladkak <codingwalaladaka@gmail.com>
2023-08-18 05:58:32 +0530

    fu8nction-11.js

---
+++ function-11.js
@@ -0,0 +1,12 @@
+function onePluseAvg(x, y){
+    console.log("Done")
+    return Math.round> (x + y)/2
+}
+
+let a = 5;
+let b = 4;
+let c = 7;
+
+console.log("two plus Average of a and b", onePluseAvg(a, b));
+console.log("five plus Average of a and c", onePluseAvg(a, c));
+console.log("athe plus Average of b and c", onePluseAvg(b, c));
