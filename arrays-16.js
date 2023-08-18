3a424c020e642f8f93eb9253dca0168654503638
codingwalaladkak <codingwalaladaka@gmail.com>
2023-08-18 05:33:28 +0530

    arrays-16.js

---
+++ arrays-16.js
@@ -0,0 +1,23 @@
+let marks_class_12 = [91, 82, 94, 54, null, "not persent"]
+
+
+// console.log(marks_class_12[0])
+// console.log(marks_class_12[1])
+// console.log(marks_class_12[2])
+// console.log(marks_class_12[3])
+// console.log(marks_class_12[4])
+// console.log(marks_class_12[5])
+// console.log(marks_class_12[6]) //wii be undefing becouse the 6th number value is nt exist
+
+// for (let i=0; i <= 6; i++){
+// console.log(marks_class_12[i])
+// }
+
+console.log("The length of marks class 12 - ", marks_class_12.length)
+
+marks_class_12[6] = 89 //Adding the new value to the array
+marks_class_12[2] = 65 //chenging the value into new values
+
+console.log(marks_class_12);
+console.log(marks_class_12);
+console.log(typeof marks_class_12);
