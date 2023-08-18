b6d9f82f69c9173f80aa70ea88e5f256312c08ba
codingwalaladkak <codingwalaladaka@gmail.com>
2023-08-18 07:30:03 +0530

    exercise-1-22.js

--- exercise-1-22.js
+++ exercise-1-22.js
@@ -4,16 +4,50 @@
 //100 - (no of guesses) is the score of the user the program is expected to terminate once the number is gussed. the number should be between 1 - 100
 
 
+var winningNumber;  // The random number to guess
+var numberOfWins = 0;  // Counter for the number of wins
 
-// Genrating random number between 0 to 100
-let randomNumber = Math.random() * 100;
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 101);  // Generate a random number between 0 and 100
+}
 
-//round to the random number to whole number
-let roundNumber = Math.floor(randomNumber);
+function playGame(guess) {
+    if (guess === winningNumber) {
+        numberOfWins++;
+        console.log("Congratulations! Your guess is correct.");
+        console.log("Total wins: " + numberOfWins);
+        return true;  // Indicate a win
+    } else {
+        console.log("Sorry, your guess is incorrect. Try again.");
+        return false;  // Indicate a loss
+    }
+}
 
-// Store the rounded number in a new variable
-var randomBetween0And100 = roundNumber;
+// Main loop
+while (true) {
+    winningNumber = generateRandomNumber();
+    console.log("New game! Try to guess the number between 0 and 100.");
 
-console.log("this is and random number:",randomNumber);
-console.log("this wan an random number but update as an whole number:",randomBetween0And100);
+    while (true) {
+        var userGuess = parseInt(prompt("Enter your guess:"));
 
+        if (isNaN(userGuess)) {
+            console.log("Please enter a valid number.");
+        } else if (userGuess < 0 || userGuess > 100) {
+            console.log("Please enter a number between 0 and 100.");
+        } else {
+            if (playGame(userGuess)) {
+                break;  // Exit the guessing loop if the user wins
+            }
+        }
+    }
+
+    var playAgain = prompt("Do you want to play again? (yes or no)").toLowerCase();
+    if (playAgain !== "yes") {
+        console.log("Thanks for playing!");
+        break;  // Exit the main loop if the user doesn't want to play again
+    }
+}
+
+
+console.log(winningNumber)
