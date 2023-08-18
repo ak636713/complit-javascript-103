b1d3901fb33e7fe2991b13658ce2f079e84e6663
codingwalaladkak <codingwalaladaka@gmail.com>
2023-08-18 05:32:51 +0530

    array-method17.js

---
+++ array-method17.js
@@ -0,0 +1,38 @@
+// Aeeay Methods    
+//  let n = [1, 2, 3]
+//  console.log(n.toString())
+
+// let n = [1, 2, 13]
+// console.log(n.join('-'))
+
+// let n = [1, 2, 13]
+// s = n.pop() //pop returns the popped value
+// console.log(n, s)
+
+// let n = [1, 2, 13]
+// s = n.push(56) //push returns A new array length
+// console.log(n, s)
+
+// let n = [1, 2, 13]
+// s = n.shift() // Removes an element from the starrt of the array
+// console.log(n, s)
+
+// let n = [1, 2, 13]
+// s = n.unshift(58) // Removes an element from the starrt of the array
+// console.log(n, s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
