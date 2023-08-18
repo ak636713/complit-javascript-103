d1add581507a04036acfc11a01e2f40e6a46f142
codingwalaladkak <codingwalaladaka@gmail.com>
2023-08-18 06:01:43 +0530

    string-13.js

---
+++ string-13.js
@@ -0,0 +1,26 @@
+// let Name = "Ajay"
+// // console.log(Name.length)
+// console.log(Name[0])
+// console.log(Name[1])
+// console.log(Name[2])
+// console.log(Name[3])
+
+
+// let Friend = 'raj'
+// console.log(Friend)
+
+// // template literals 
+
+// let boy1 = "ajay"
+// let boy2 = "sorve"
+// // sorve is friend of ajay
+// let sentence = `${boy1} is a friend of ${boy2}`
+// console.log(sentence)
+
+
+// Escape Sequence characters
+
+// let frut = 'bana\'na'
+// console.log(frut.length)
+let frut = 'bana\nna'
+console.log(frut)
