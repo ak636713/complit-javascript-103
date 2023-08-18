a64712d9562d48fa030de63497c79b6d7f878294
codingwalaladkak <codingwalaladaka@gmail.com>
2023-08-18 05:35:48 +0530

    array_practice_set_21.js

---
+++ chapter-3-practice-set-12.js
@@ -0,0 +1,39 @@
+// let marks = {
+//     ajay: 90,
+//     sub: 30,
+//     raj: 12,
+//     loki: 1,
+// }
+
+//problem no 1
+
+// for(let i=0;i<Object.keys(marks).length;i++){
+//     console.log("The marks of " + Object.keys(marks)[i] + " are " + marks[ Object.keys(marks)[i]])
+// }
+
+
+//problem no 2
+
+// for(let key in marks){
+//     console.log("The marks of " + key + " are " + marks[key])
+// }
+
+
+//problem no 3
+
+// let cn = 4
+// let i 
+// while (i != cn) {
+//     console.log("Try again")
+//     i = prompt("Enter a number")
+// }
+// console.log("you have entered a correct number")
+
+
+//problem no 4
+
+const mean = (a, b, c, d) =>{
+    return (a + b + c + d ) / 4
+}
+
+console.log(mean(4,5,8,7))
