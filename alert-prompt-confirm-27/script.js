bc798776bcc6d1b224677d3c31bf7d9db8ce7ba2
codingwalaladkak <codingwalaladaka@gmail.com>
2023-08-20 08:39:04 +0530

    alert-prompt-confirm-27

---
+++ alert-prompt-confirm-27/script.js
@@ -0,0 +1,20 @@
+let al = alert("Please enter your name!")
+
+console.log(al)
+
+let message = prompt("hy enter your name")
+
+// message = Number.parseInt(message)
+// alert("you enterd a of type "+ (typeof a))
+
+let w = confirm("do you want to enter your name")
+
+if(w)
+{
+document.write(message)
+
+}
+
+else{
+    document.write("please allow me to write you name here ")   
+}
