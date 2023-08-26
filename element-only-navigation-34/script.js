5f081f8a97a1402b7fd8a5747486bd711a24476b
codingwalaladkak <codingwalaladaka@gmail.com>
2023-08-26 07:27:00 +0530

    element-only-navigation-34/index.html

---
+++ element-only-navigation-34/script.js
@@ -0,0 +1,12 @@
+const changeBodyRed = () => {
+    document.body.firstElementChild.style.background = "red";
+}
+
+console.log(changeBodyRed());
+
+
+
+
+let b = document.body
+console.log("First child of b is: ", b.firstChild);
+console.log("First Element child of b is: ", b.firstElementChild);
