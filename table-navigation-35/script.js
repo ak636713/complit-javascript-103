f7e98acd164b9781ae7a23109768bff2da82457c
codingwalaladkak <codingwalaladaka@gmail.com>
2023-08-26 08:20:34 +0530

    table-navigation-35
    git commit -m table-navigation-35

--- table-navigation-35/script.js
+++ table-navigation-35/script.js
@@ -3,3 +3,8 @@ let t = document.body.firstElementChild.firstElementChild
 console.log(t)
 console.log(t.rows)
 console.log(t.caption)
+console.log(t.tHeadfirstElementChild)
+console.log(t.tFoot)
+console.log(t.tBodis)
+
+console.log(t.rows[0].rowIndex)
