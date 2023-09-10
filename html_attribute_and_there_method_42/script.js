c04d08c9f1310114476b223af3b75dd172419b49
codingwalaladkak <codingwalaladaka@gmail.com>
2023-09-10 22:10:59 +0530

    html_attribute_and_there_method_42

---
+++ html_attribute_and_there_method_42/script.js
@@ -0,0 +1,10 @@
+let a = first.getAttribute("class")
+
+console.log(a)
+console.log(first.hasAttribute("style"))
+console.log(first.hasAttribute("class"))
+
+// first.setAttribute("hidden", "true")
+first.setAttribute("class", "true ajay")
+first.Attribute()
+
